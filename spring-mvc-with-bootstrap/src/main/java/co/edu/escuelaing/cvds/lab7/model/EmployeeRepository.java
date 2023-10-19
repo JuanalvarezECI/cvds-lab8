873594026b6240c0bd9bb8c95f73531b66994060
JuanalvarezECI <143049108+JuanalvarezECI@users.noreply.github.com>
2023-10-19 02:49:26 -0500

    Delete spring-mvc-with-bootstrap directory

--- spring-mvc-with-bootstrap/src/main/java/co/edu/escuelaing/cvds/lab7/model/EmployeeRepository.java
+++
@@ -1,6 +0,0 @@
-package co.edu.escuelaing.cvds.lab7.model;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<User, Long> {
-}
-
